ff67739b3048c5eb6d789dccf6626e38bd0861fb
lucasangoston <lucasangostonpro@gmail.com>
2022-12-31 00:44:37 +0100

    fix

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,11 +1,16 @@
-import { Button, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import { blue } from '@mui/material/colors';
 import * as React from 'react';
+import styles from '../contact/style/contact.module.css';
 
 export function Contact() {
+  function displayContactMessage() {
+    console.log('click');
+  }
+
   return (
-    <div>
+    <div onClick={displayContactMessage} className={styles.contact}>
       <Grid container direction="row">
         <Avatar sx={{ bgcolor: blue[500] }} aria-label="recipe"></Avatar>
         <ol style={{ marginLeft: '10px' }}>
